e2be7ea9c9d0bbfa25c495d2c5a76e1dccc44ef6
root <root@DESKTOP-JUC2QBO>
2023-10-24 14:04:13 +0100

    add scheduler

--- app.py
+++ app.py
@@ -1,8 +1,26 @@
 from flask import Flask, render_template
 from get_fixtures import get_fixtures
+from apscheduler.schedulers.background import BackgroundScheduler
+
+FIXTURES = None
 app = Flask(__name__)
+
+def fixtures_call():
+    print("here")
+    global FIXTURES
+    FIXTURES = get_fixtures()
+
+
+fixtures_call()
+
+sched = BackgroundScheduler(daemon=True)
+sched.add_job(fixtures_call,'interval',minutes=60)
+sched.start()
 
 @app.route("/")
 def hello():
-    results = get_fixtures()
+    results = FIXTURES
     return render_template("home.html", result = results)
+
+if __name__ == "__main__":
+    app.run()
